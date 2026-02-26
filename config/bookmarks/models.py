7b30d98364a4018f0f17fd86466ed08b5d8cffdf
Jackson <lewisja@otc.edu>
2026-02-26 14:37:39 -0600

    Created ActivityLog model

--- config/bookmarks/models.py
+++ config/bookmarks/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -15,3 +17,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+    
+class ActivityLog(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    action = models.CharField(max_length=160)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.action} on {self.content_object} at {self.timestamp:%H:%M}'
