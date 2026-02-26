53d84281a89a139fcf92666bcc1ba41c625fd937
Jackson <lewisja@otc.edu>
2026-02-26 14:15:57 -0600

    Created tag and article models

--- config/bookmarks/models.py
+++ config/bookmarks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    notes = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
