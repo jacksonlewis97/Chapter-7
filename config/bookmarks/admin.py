6f1d558090b1c2fe7cec000cd4cf52553a1629f5
Jackson <lewisja@otc.edu>
2026-02-26 14:22:47 -0600

    Initial migration and admin setup

--- config/bookmarks/admin.py
+++ config/bookmarks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Tag, Article
 
-# Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'created_at']
+    filter_horizontal = ['tags']
